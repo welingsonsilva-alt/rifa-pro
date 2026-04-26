685125e4b161a1d26200877ab60f8ef5ddffaaf5
Welingson Silva <welingson.silva@gmail.com>
2026-04-26 20:05:17 -0300

    Fix: Explicit express export and build config

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const { createClient } = require('@supabase/supabase-js');
-const path = require('path');
 
 const app = express();
 app.use(cors());
